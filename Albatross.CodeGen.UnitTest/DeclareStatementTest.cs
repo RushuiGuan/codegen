bc845ab6fcba6ef590393fa42302f746689fdca0
Rushui Guan <rushui@outlook.com>
2018-02-02 18:07:10 -0500

    unit testing yield functionality

--- Albatross.CodeGen.UnitTest/DeclareStatementTest.cs
+++ Albatross.CodeGen.UnitTest/DeclareStatementTest.cs
@@ -1,5 +1,6 @@
 ﻿using Albatross.CodeGen.Database;
 using Albatross.CodeGen.SqlServer;
+using Albatross.CodeGen.UnitTest.Mocking;
 using NUnit.Framework;
 using SimpleInjector;
 using System;
@@ -10,19 +11,19 @@ using System.Threading.Tasks;
 
 namespace Albatross.CodeGen.UnitTest {
 	[TestFixture]
-	public class DeclareStatementTest : TestBase{
-		Container Container;
-
-		[OneTimeSetUp]
-		public void Setup() {
-			Container = GetContainer();
-			Container.Verify();
+	public class DeclareStatementTest {
+		static IEnumerable<TestCaseData> GetTestCase() {
+			return new TestCaseData[] {
+				new TestCaseData(SymbolTable.Table, new SqlQueryOption()){ ExpectedResult="insert into [test].[Symbol] ([SyCode], [CuID], [OutShares], [CoID], [SnID])\r\nvalues (@syCode, @cuID, @outShares, @coID, @snID)" },
+				new TestCaseData(ContactTable.Table, new SqlQueryOption()){ ExpectedResult="insert into [test].[Contact] ([Domain], [Login], [FirstName], [LastName], [MiddleName], [Gender], [Cell], [Address], [Created], [CreatedBy], [Modified], [ModifiedBy])\r\nvalues (@domain, @login, @firstName, @lastName, @middleName, @gender, @cell, @address, @created, @createdBy, @modified, @modifiedBy)" },
+				new TestCaseData(ContactTable.Table, new SqlQueryOption{ Variables={ { "@user", "varchar(100)"} }, Expressions={ { "created", "getdate()"}, {"modified", "getdate()"}, { "createdBy", "@user"},{ "modifiedBy", "@user"} } }){ ExpectedResult="insert into [test].[Contact] ([Domain], [Login], [FirstName], [LastName], [MiddleName], [Gender], [Cell], [Address], [Created], [CreatedBy], [Modified], [ModifiedBy])\r\nvalues (@domain, @login, @firstName, @lastName, @middleName, @gender, @cell, @address, getdate(), @user, getdate(), @user)" },
+			};
 		}
 
-		public string Build(DatabaseObject @params) {
-			DeclareStatement handle = Container.GetInstance<DeclareStatement>();
-			return null;
-			//return handle.Build(new StringBuilder(), @params, null, null).ToString();
+		[TestCaseSource(nameof(GetTestCase))]
+		public string BuildDeclareStatement(DatabaseObject table, SqlQueryOption option) {
+			StringBuilder sb = new StringBuilder();
+			return sb.ToString();
 		}
 	}
 }
