cea2d815afbf16ae93fa4a8d9f293e42e8926823
rushui <rushui@outlook.com>
2019-03-30 19:38:46 -0400

    update

--- src/Albatross.CodeGen.Autofac/pack.cs
+++ src/Albatross.CodeGen.Autofac/pack.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Albatross.CodeGen.Core;
 using Autofac;
 
 namespace Albatross.CodeGen.Autofac {
 	public class Pack {
 		public void Register(ContainerBuilder containerBuilder) {
-		}
+			var asm = typeof(Albatross.CodeGen.Core.ICodeGenerator).Assembly;
+			}
 	}
 }
