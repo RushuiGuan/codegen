3db03196ca36f5d45d2963f0ff6ea20d8da8c295
Rushui Guan <rushui@outlook.com>
2019-06-19 00:52:07 -0400

    remove unused projects

--- src/Albatross.CodeGen.CSharp/Model/ParameterModifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Albatross.CodeGen.CSharp.Model {
-	public enum ParameterModifier {
-		In, Out, Ref
-	}
-}
--- src/Albatross.CodeGen.SqlServer/readme.md
+++
@@ -1,7 +0,0 @@
-﻿code generation with sources coming from sql server.
-
-Available source:
-DataObject
-
-Available Option:
-SqlQueryOption
