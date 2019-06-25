3a2276e1930dacab493f46a7067d5dbf1443d009
Rushui Guan <rushui@outlook.com>
2019-06-24 22:47:20 -0400

    add code cleanup

--- scripts/publish-nuget.ps1
+++ scripts/publish-nuget.ps1
@@ -2,13 +2,19 @@ $ErrorActionPreference = "stop";
 Import-Module app-dev;
 $app_root = Get-Item $PSScriptRoot\..;
 
+Remove-NugetCache Albatross.CodeGen*;
+
 $array = @(
-    "$app_root\src\Albatross.CodeGen",
-    "$app_root\src\Albatross.CodeGen.Autofac"
+	"$app_root\src\Albatross.CodeGen",
+	"$app_root\src\Albatross.CodeGen.Autofac"
 );
 
 $local = (Get-NugetLocal);
+$config = "debug";
+$array | ForEach-Object{ 
+	Invoke-DotnetClean -csproj $_ -config $config;
+}
 $array | ForEach-Object {
-    $out = Get-Path $local, (get-item $_).Name;
-    Invoke-DotnetPack -csproj $_  -config debug  -out $out;
+	$out = Get-Path $local, (get-item $_).Name;
+	Invoke-DotnetPack -csproj $_  -config $config -out $out;
 }
