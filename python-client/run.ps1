59176fc29af5005a27cdb5ccfc39e91abc4f87ab
rushuiguan <rushui@gmail.com>
2025-11-09 03:02:39 -0500

    continue to work on python codegen

---
+++ python-client/run.ps1
@@ -0,0 +1,21 @@
+Get-ChildItem $PSScriptRoot/*.py | ForEach-Object {
+    $line = Get-Content $_.FullName -TotalCount 1;
+    if ($line -eq '# @generated') {
+        Write-Host "Removing generated file $($_.FullName)"
+        Remove-Item $_.FullName
+    }
+}
+
+
+
+$root = git rev-parse --show-toplevel
+$location = Get-Location
+
+Set-Location $root/Albatross.CodeGen.CommandLine
+dotnet build
+dotnet run --no-launch-profile -- settings-schema --file ../python-client/codegen/codegen-settings.schema.json
+dotnet run --no-launch-profile -- py dto  -p ../Test.Dto/Test.Dto.csproj -s ../python-client/codegen/codegen-settings.json -o ../python-client/
+dotnet run --no-launch-profile -- model dto -p ../Test.Dto/Test.Dto.csproj -s ../python-client/codegen/codegen-settings.json -o ../python-client/codegen/models/
+dotnet run --no-launch-profile -- model controller -p ../Test.WebApi/Test.WebApi.csproj -s ../python-client/codegen/codegen-settings.json -o ../python-client/codegen/models/
+dotnet run --no-launch-profile -- py web-client -p ../Test.WebApi/Test.WebApi.csproj -s ../python-client/codegen/codegen-settings.json -o ../python-client/
+Set-Location $location
