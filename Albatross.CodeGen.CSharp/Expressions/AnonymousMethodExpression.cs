9814f6b4c67bf560c5c5b688a2c2e7781d01216e
rushuiguan <rushui@gmail.com>
2025-12-07 10:09:38 -0500

    add anonymous method

---
+++ Albatross.CodeGen.CSharp/Expressions/AnonymousMethodExpression.cs
@@ -0,0 +1,48 @@
+using Albatross.CodeGen.CSharp.Declarations;
+using Albatross.CodeGen.Syntax;
+using Albatross.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Albatross.CodeGen.CSharp.Expressions {
+	public record class AnonymousMethodExpression: SyntaxNode , IExpression {
+		public IEnumerable<ParameterDeclaration> Parameters { get; init; } = Array.Empty<ParameterDeclaration>();
+		public IEnumerable<IExpression> Body { get; init; } = [];
+		public override TextWriter Generate(TextWriter writer) {
+			if (!Parameters.Any()) {
+				writer.Append("()");
+			}else if (Parameters.Count() == 1 && Object.Equals(Parameters.First().Type, Defined.Types.Var)){
+				// omit parantheses and type for single var parameter
+				writer.Code(Parameters.First().Name);
+			}else {
+				writer.WriteItems(Parameters, ", ", (w, param) => {
+					if (object.Equals(param.Type, Defined.Types.Var)) {
+						w.Code(param.Name);
+					} else {
+						w.Code(param);
+					}
+				}, "(", ")");
+			}
+			writer.Append(" =>");
+			if(!Body.Any()) {
+				writer.Append(" { }");
+			} else if (Body.Count() == 1) {
+				writer.Space().Code(Body.First());
+			} else {
+				using var scope = writer.BeginScope();
+				scope.Writer.Code(new CompositeExpression(Body));
+			}
+			return writer;
+		}
+
+		public override IEnumerable<ISyntaxNode> Children {
+			get {
+				var list = new List<ISyntaxNode>(Parameters);
+				list.AddRange(Body);
+				return list;
+			}
+		}
+	}
+}
---
+++ Albatross.CodeGen.UnitTest/CSharp/TestAnonymousMethodExpression.cs
@@ -0,0 +1,108 @@
+using Albatross.CodeGen.CSharp;
+using Albatross.CodeGen.CSharp.Declarations;
+using Albatross.CodeGen.CSharp.Expressions;
+using Albatross.CodeGen.Syntax;
+using Albatross.Testing;
+using System.IO;
+using Xunit;
+
+namespace Albatross.CodeGen.UnitTest.CSharp {
+	public class TestAnonymousMethodExpression {
+		[Fact]
+		public void TestEmpty() {
+			var expression = new AnonymousMethodExpression();
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("() => { }", text);
+		}
+		[Fact]
+		public void TestSingleTypeParameter() {
+			var expression = new AnonymousMethodExpression {
+				Parameters = [
+					new ParameterDeclaration {
+						Type = new TypeExpression("int"),
+						Name = new IdentifierNameExpression("x")
+					}
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("(int x) => { }", text);
+		}
+		[Fact]
+		public void TestSingleVarParameter() {
+			var expression = new AnonymousMethodExpression {
+				Parameters = [
+					new ParameterDeclaration {
+						Type = Defined.Types.Var,
+						Name = new IdentifierNameExpression("x")
+					}
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("x => { }", text);
+		}
+		[Fact]
+		public void TestMultipleTypedParameter() {
+			var expression = new AnonymousMethodExpression {
+				Parameters = [
+					new ParameterDeclaration {
+						Type = Defined.Types.Int,
+						Name = new IdentifierNameExpression("x")
+					},
+					new ParameterDeclaration {
+						Type = Defined.Types.Int,
+						Name = new IdentifierNameExpression("y")
+					}
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("(int x, int y) => { }", text);
+		}
+		[Fact]
+		public void TestMultipleVarParameter() {
+			var expression = new AnonymousMethodExpression {
+				Parameters = [
+					new ParameterDeclaration {
+						Type = Defined.Types.Var,
+						Name = new IdentifierNameExpression("x")
+					},
+					new ParameterDeclaration {
+						Type = Defined.Types.Var,
+						Name = new IdentifierNameExpression("y")
+					}
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("(x, y) => { }", text);
+		}
+		[Fact]
+		public void TestSingleExpression() {
+			var expression = new AnonymousMethodExpression {
+				Body = [
+					new StringLiteralExpression("hello")
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			Assert.Equal("() => \"hello\"", text);
+		}
+		[Fact]
+		public void TestMultilineBody() {
+			var expression = new AnonymousMethodExpression {
+				Body = [
+					new AssignmentExpression{
+						Left = new VariableDeclaration{
+							Type = Defined.Types.Int,
+							Identifier = new IdentifierNameExpression("x"),
+						},
+						Expression = new IntLiteralExpression(10)
+					}.Terminate(),
+					new ReturnExpression{
+						Expression = new IdentifierNameExpression("x")
+					}.Terminate(),
+					new NewLineExpression(),
+				]
+			};
+			var text = new StringWriter().Code(expression).ToString();
+			"() => {\tint x = 10;\n\treturn x;\n}".EqualsIgnoringLineEndings(text);
+		}
+	}
+}
