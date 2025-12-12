9c3821a9b26f2f4162304d3e0f98580ad0ba931b
rushuiguan <rushui@gmail.com>
2025-12-11 23:54:54 -0500

    name change

--- Albatross.CodeGen.CSharp/Expressions/NewObjectExpression.cs
+++ Albatross.CodeGen.CSharp/Expressions/NewObjectExpression.cs
@@ -5,11 +5,11 @@ using System.IO;
 namespace Albatross.CodeGen.CSharp.Expressions {
 	public record class NewObjectExpression : SyntaxNode, IExpression {
 		public required ITypeExpression Type { get; init; }
-		public ListOfArguments ListOfArguments { get; init; } = new();
+		public ListOfArguments Arguments { get; init; } = new();
 
 		public override TextWriter Generate(TextWriter writer)
-			=> writer.Code(Defined.Keywords.New).Code(Type).Code(ListOfArguments);
+			=> writer.Code(Defined.Keywords.New).Code(Type).Code(Arguments);
 
-		public override IEnumerable<ISyntaxNode> Children => [Type, ListOfArguments];
+		public override IEnumerable<ISyntaxNode> Children => [Type, Arguments];
 	}
 }
