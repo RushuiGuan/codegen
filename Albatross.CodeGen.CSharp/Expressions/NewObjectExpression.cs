ff043891409540356f57b175de3f14377114be46
rushuiguan <rushui@gmail.com>
2025-12-15 00:57:59 -0500

    update newObjectexpression

--- Albatross.CodeGen.CSharp/Expressions/NewObjectExpression.cs
+++ Albatross.CodeGen.CSharp/Expressions/NewObjectExpression.cs
@@ -1,15 +1,29 @@
 ﻿using Albatross.CodeGen.Syntax;
+using Albatross.Text;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Albatross.CodeGen.CSharp.Expressions {
 	public record class NewObjectExpression : SyntaxNode, IExpression {
 		public required ITypeExpression Type { get; init; }
 		public ListOfArguments Arguments { get; init; } = new();
+		public IEnumerable<AssignmentExpression> Initializers { get; init; } = [];
 
-		public override TextWriter Generate(TextWriter writer)
-			=> writer.Code(Defined.Keywords.New).Code(Type).Code(Arguments);
+		public override TextWriter Generate(TextWriter writer) {
+			writer.Code(Defined.Keywords.New).Code(Type).Code(Arguments);
+			if (Initializers.Any()) {
+				using var scope = writer.BeginScope();
+				foreach (var initializer in Initializers) {
+					scope.Writer.Code(initializer).AppendLine(",");
+				}
+			}
+			return writer;
+		}
 
-		public override IEnumerable<ISyntaxNode> Children => [Type, Arguments];
+		public override IEnumerable<ISyntaxNode> Children => new List<ISyntaxNode>(Initializers) {
+			Type,
+			Arguments,
+		};
 	}
 }
