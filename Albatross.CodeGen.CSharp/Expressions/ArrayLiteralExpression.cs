2288e4d3c5d308f3564911b8ad643b58dd357b1e
rushuiguan <rushui@gmail.com>
2025-12-15 01:18:48 -0500

    fix arrayliteralexpression

--- Albatross.CodeGen.CSharp/Expressions/ArrayLiteralExpression.cs
+++ Albatross.CodeGen.CSharp/Expressions/ArrayLiteralExpression.cs
@@ -1,15 +1,11 @@
 using Albatross.CodeGen.Syntax;
-using Albatross.Text;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Albatross.CodeGen.CSharp.Expressions {
-	public record class ArrayLiteralExpression : LiteralExpression {
-		public ListOfSyntaxNodes<IExpression> Items { get; init; } = new ListOfSyntaxNodes<IExpression>();
-		public override TextWriter Generate(TextWriter writer) {
-			writer.Append("[").Code(Items).Append("]");
-			return writer;
+	public record class ArrayLiteralExpression : ListOfSyntaxNodes<LiteralExpression>, IExpression {
+		public ArrayLiteralExpression(params IEnumerable<LiteralExpression> items) : base(items) {
+			Prefix = "[";
+			PostFix = "]";
 		}
-		public override IEnumerable<ISyntaxNode> Children => [Items];
 	}
 }
