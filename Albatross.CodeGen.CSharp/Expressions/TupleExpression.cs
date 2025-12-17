3f4de0ff2ead466fb3cf88a951cbca72d13aef5a
rushuiguan <rushui@outlook.com>
2025-12-17 13:18:33 -0500

    fix tuple expression

--- Albatross.CodeGen.CSharp/Expressions/TupleExpression.cs
+++ Albatross.CodeGen.CSharp/Expressions/TupleExpression.cs
@@ -1,28 +1,9 @@
-using Albatross.Text;
-using System.Collections.Generic;
-using System.IO;
-
 namespace Albatross.CodeGen.CSharp.Expressions {
-	public record class TupleExpression : CodeNode, IExpression {
-		public IExpression[] Items { get; init; }
-		public override IEnumerable<ICodeNode> Children => Items;
-		public bool LineBreak { get; set; }
-
-		public TupleExpression(params IExpression[] items) {
-			this.Items = items;
-		}
-
-		public override TextWriter Generate(TextWriter writer) {
-			writer.Append('(');
-			if (LineBreak) {
-				writer.AppendLine();
-			}
-			writer.WriteItems(Items, ", ", (w, item) => w.Code(item));
-			if (LineBreak) {
-				writer.AppendLine();
-			}
-			writer.Append(')');
-			return writer;
+	public record class TupleExpression : ListOfNodes<IExpression>, IExpression {
+		public TupleExpression(params IExpression[] items) : base(items) {
+			Prefix = "(";
+			PostFix = ")";
+			Separator = ", ";
 		}
 	}
 }
