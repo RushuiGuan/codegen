2f39eed74a763b61b0611a5b4ac8afc1ae0b1813
rushuiguan <rushui@gmail.com>
2025-12-24 00:05:47 -0500

    fix the unit test

--- Albatross.CodeGen.UnitTest/CSharp/TestDefaultTypeConverter.cs
+++ Albatross.CodeGen.UnitTest/CSharp/TestDefaultTypeConverter.cs
@@ -16,7 +16,7 @@ public class SqlRepository<T> : IRepository<T> {}
 ";
 			var compilation = await code.CreateNet8CompilationAsync();
 			var symbol = compilation.GetRequiredSymbol("IRepository`1").Construct(compilation.GetRequiredSymbol("MyClass"));	
-			var converter = new DefaultTypeConverter(compilation);
+			var converter = new DefaultTypeConverter();
 			var expression = converter.Convert(symbol);
 			var text = new StringWriter().Code(expression).ToString();
 			Assert.Equal("IRepository<MyClass>", text);
