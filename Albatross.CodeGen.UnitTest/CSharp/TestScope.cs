9f6ff946b945436288fee7e3361de66f1d6925f1
rushuiguan <rushui@outlook.com>
2026-01-02 19:46:31 -0500

    wip

---
+++ Albatross.CodeGen.UnitTest/CSharp/TestScope.cs
@@ -0,0 +1,16 @@
+﻿using Albatross.Text;
+using System.IO;
+using Xunit;
+
+namespace Albatross.CodeGen.UnitTest.CSharp {
+	public class TestScope {
+		[Fact]
+		public void TestNormal() {
+			var writer = new StringWriter();
+			using (var scope = writer.Append("test").BeginScope("{", "}")) {
+				scope.Writer.Append("test");
+			}
+			Assert.Equal("test{\r\n\ttest\n}", writer.ToString());
+		}
+	}
+}
