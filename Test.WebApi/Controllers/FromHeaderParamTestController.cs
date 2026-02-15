ceff21b9146ad71cfd511ee111c62cc5909fedfb
Rushui Guan <rushui@outlook.com>
2026-02-15 09:09:03 -0700

    regen test.proxy and test.client

--- Test.WebApi/Controllers/FromHeaderParamTestController.cs
+++ Test.WebApi/Controllers/FromHeaderParamTestController.cs
@@ -5,15 +5,7 @@ namespace Test.WebApi.Controllers {
 	[Route("api/from-header-param-test")]
 	[ApiController]
 	public class FromHeaderParamTestController : ControllerBase {
-		public class Context {
-			[FromHeader(Name = "id")]
-			public string? Id { get; set; }
-
-			[FromHeader(Name = "type")]
-			public string? Type { get; set; }
-		}
-
 		[HttpGet]
-		public void OmitFromHeaderParameters([FromHeader(Name = "n")] string name, [FromHeader] Context context) { }
+		public void OmitFromHeaderParameters([FromHeader(Name = "n")] string name) { }
 	}
 }
